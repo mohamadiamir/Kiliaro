ac28cab58c9952920abd3bea6e5b9297d31b2d8c
amir mohamadi <mohamadi.dev@gmail.com>
2022-08-26 06:44:57 -0700

    Initial Commit

---
+++ Kiliaro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kiliaro
+//
+//  Created by Amir Mohamadi on 8/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
